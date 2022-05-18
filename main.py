72fe9cc3eada6b517fd28907ddd2a3f867c1f975
Emil Sležis <emilslezis@gmail.com>
2022-05-18 13:55:40 +0300

    add obectInteractionHandler

---
+++ main.py
@@ -0,0 +1,44 @@
+﻿import viz
+import vizact
+import vizinfo
+
+viz.setMultiSample(4)
+viz.fov(60)
+viz.go()
+
+viz.clearcolor(viz.SKYBLUE)
+
+soccerball = viz.addChild('soccerball.osgb')
+basketball = viz.addChild('basketball.osgb')
+volleyball = viz.addChild('volleyball.osgb')
+
+soccerball.setPosition([-0.5,2,1.5])
+basketball.setPosition([0,2,1.5])
+volleyball.setPosition([0.5,2,1.5])
+
+info = vizinfo.InfoPanel('Test description', align=viz.ALIGN_RIGHT_TOP, icon=False)
+info.visible(0)
+
+viz.mouse(viz.OFF)
+object = viz.pick()
+
+def pickObject():
+    object = viz.pick()
+    if object.valid():
+        pos = object.getPosition()
+
+        info.visible(1)
+
+        if object.valid and object == soccerball:
+          info.setTitle("Soccerball")
+
+        if object.valid and object == basketball:
+          info.setTitle("Basketball")
+
+        if object.valid and object == volleyball:
+          info.setTitle("Volleyball")
+       
+    else:
+      info.visible(0)
+
+vizact.onmousedown(viz.MOUSEBUTTON_LEFT, pickObject)
